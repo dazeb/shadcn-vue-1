d714e17f98e5f25ce56465279bfc73c83c73c854
dazeb <daz@dazeb.dev>
2024-08-04 09:41:56 +0100

    commit
    Signed-off-by: dazeb <daz@dazeb.dev>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -71,14 +71,6 @@ const routes = [
     name: "Components",
     path: "/docs/components/accordion",
   },
-  {
-    name: "Themes",
-    path: "/themes",
-  },
-  {
-    name: "Examples",
-    path: "/examples/dashboard",
-  },
 ];
 
 const pages = [
@@ -142,69 +134,43 @@ const isOpen = ref(false);
 <template>
   <div class="flex min-h-screen flex-col bg-background">
     <header class="sticky top-0 z-40 border-b border-border bg-background">
-      <div
-        class="mx-auto flex h-[58px] max-w-8xl items-center justify-between p-4"
-      >
+      <div class="mx-auto flex h-[58px] max-w-8xl items-center justify-between p-4">
         <div class="flex gap-6 md:gap-8">
           <RouterLink to="/" class="text-md font-bold"> shadcn-vue </RouterLink>
 
-          <nav
-            class="hidden items-center justify-center gap-6 md:flex"
-            v-for="route in routes"
-            :key="route.name"
-          >
-            <RouterLink
-              :to="route.path"
-              class="text-sm font-medium text-muted-foreground transition-colors dark:hover:text-gray-300"
-              :class="{
+          <nav class="hidden items-center justify-center gap-6 md:flex" v-for="route in routes" :key="route.name">
+            <RouterLink :to="route.path"
+              class="text-sm font-medium text-muted-foreground transition-colors dark:hover:text-gray-300" :class="{
                 'font-semibold !text-foreground': $route.path === route.path,
-              }"
-            >
+              }">
               {{ route.name }}
             </RouterLink>
           </nav>
           <nav class="hidden items-center justify-center gap-6 md:flex">
-            <a
-              href="https://github.com/ahmedmayara/shadcn-vue"
-              target="_blank"
-              class="text-sm font-medium text-muted-foreground transition-colors dark:hover:text-gray-300"
-            >
+            <a href="https://github.com/ahmedmayara/shadcn-vue" target="_blank"
+              class="text-sm font-medium text-muted-foreground transition-colors dark:hover:text-gray-300">
               Github
             </a>
           </nav>
         </div>
 
         <div class="hidden flex-1 items-center justify-end space-x-4 md:flex">
-          <Button
-            @click="isOpen = true"
-            variant="outline"
-            class="hidden w-64 lg:flex lg:items-center lg:justify-between"
-          >
+          <Button @click="isOpen = true" variant="outline"
+            class="hidden w-64 lg:flex lg:items-center lg:justify-between">
             <span class="text-muted-foreground"> Search for anything... </span>
             <div class="flex items-center">
               <Kbd> <span>⌘</span>K </Kbd>
             </div>
           </Button>
 
-          <div
-            class="flex items-center space-x-1"
-            v-for="link in links"
-            :key="link.name"
-          >
+          <div class="flex items-center space-x-1" v-for="link in links" :key="link.name">
             <a :href="link.href" target="_blank" class="text-foreground">
               <component :is="link.icon" class="h-5 w-5" />
             </a>
           </div>
 
-          <button
-            class="flex items-center justify-center"
-            aria-label="Toggle dark mode"
-            @click="toggleDark()"
-          >
-            <component
-              :is="isDark ? RadixIconsSun : RadixIconsMoon"
-              class="h-5 w-5 text-foreground"
-            />
+          <button class="flex items-center justify-center" aria-label="Toggle dark mode" @click="toggleDark()">
+            <component :is="isDark ? RadixIconsSun : RadixIconsMoon" class="h-5 w-5 text-foreground" />
           </button>
 
           <button class="flex items-center justify-center md:hidden">
@@ -226,33 +192,24 @@ const isOpen = ref(false);
         <div class="flex items-center justify-center">
           <span class="text-sm">
             Built and designed by {{ " " }}
-            <a
-              href="https://twitter.com/shadcn"
-              target="_blank"
-              class="font-bold underline decoration-foreground underline-offset-4"
-            >
+            <a href="https://twitter.com/shadcn" target="_blank"
+              class="font-bold underline decoration-foreground underline-offset-4">
               shadcn
             </a>
           </span>
           <span class="ml-0.5 text-sm"> . </span>
           <span class="ml-2 text-sm">
             Ported to Vue by {{ " " }}
-            <a
-              href="https://twitter.com/ahmeedmayara"
-              target="_blank"
-              class="font-bold underline decoration-foreground underline-offset-4"
-            >
+            <a href="https://twitter.com/ahmeedmayara" target="_blank"
+              class="font-bold underline decoration-foreground underline-offset-4">
               ahmedmayara
             </a>
           </span>
           <span class="ml-0.5 text-sm"> . </span>
           <span class="ml-2 text-sm">
             The code source is available on {{ " " }}
-            <a
-              href="https://github.com/ahmedmayara/shadcn-vue"
-              target="_blank"
-              class="font-bold underline decoration-foreground underline-offset-4"
-            >
+            <a href="https://github.com/ahmedmayara/shadcn-vue" target="_blank"
+              class="font-bold underline decoration-foreground underline-offset-4">
               GitHub
             </a>
           </span>
@@ -266,70 +223,45 @@ const isOpen = ref(false);
     <CommandList>
       <CommandEmpty>No results found.</CommandEmpty>
       <CommandGroup heading="Links" class="p-2">
-        <CommandItem
-          v-for="link in routes"
-          :key="link.name"
-          :value="link.name"
-          @select="$router.push(link.path)"
-          class="py-3"
-        >
+        <CommandItem v-for="link in routes" :key="link.name" :value="link.name" @select="$router.push(link.path)"
+          class="py-3">
           <File class="mr-2 h-5 w-5" />
           <span>{{ link.name }}</span>
         </CommandItem>
       </CommandGroup>
       <CommandSeparator />
       <CommandGroup heading="Getting Started" class="p-2">
-        <CommandItem
-          v-for="page in pages"
-          :key="page.name"
-          :value="page.name"
-          @select="$router.push(page.path)"
-          class="py-3"
-        >
+        <CommandItem v-for="page in pages" :key="page.name" :value="page.name" @select="$router.push(page.path)"
+          class="py-3">
           <Circle class="mr-2 h-4 w-4" />
           <span>{{ page.name }}</span>
         </CommandItem>
       </CommandGroup>
 
       <CommandGroup heading="Components" class="p-2">
-        <CommandItem
-          v-for="component in components"
-          :key="component.attributes.name"
-          :value="component.attributes.name"
+        <CommandItem v-for="component in components" :key="component.attributes.name" :value="component.attributes.name"
           @select="
             $router.push(`/docs/components/${component.attributes.slug}`)
-          "
-          class="py-3"
-        >
+            " class="py-3">
           <Circle class="mr-2 h-4 w-4" />
           <span>{{ component.attributes.name }}</span>
         </CommandItem>
       </CommandGroup>
 
       <CommandGroup heading="Theme" class="p-2">
-        <CommandItem
-          value="light-theme"
-          @select="
-            () => {
-              isDark = false;
-              isOpen = false;
-            }
-          "
-          class="py-3"
-        >
+        <CommandItem value="light-theme" @select="() => {
+            isDark = false;
+            isOpen = false;
+          }
+          " class="py-3">
           <RadixIconsSun class="mr-2 h-5 w-5" />
           <span>Light Theme</span>
         </CommandItem>
-        <CommandItem
-          value="dark-theme"
-          @select="
-            () => {
-              isDark = true;
-              isOpen = false;
-            }
-          "
-          class="py-3"
-        >
+        <CommandItem value="dark-theme" @select="() => {
+            isDark = true;
+            isOpen = false;
+          }
+          " class="py-3">
           <RadixIconsMoon class="mr-2 h-5 w-5" />
           <span>Dark Theme</span>
         </CommandItem>
